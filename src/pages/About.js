b656358c2e7e0732fffcadb7d5772b2bd108b8fe
Daniel Ji <daniel.ji0923@gmail.com>
2024-02-03 12:02:02 -0800

    projects styled

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,8 +5,8 @@ function About(props) {
     
     return (
       <div className="about">
-        <h2>{about.name}</h2>
-        <h2>{about.email}</h2>
+        {/* <h2>{about.name}</h2> */}
+        <h2>Contact Me: <a href="mailto:daniel.ji0923@gmail.com">{about.email}</a></h2>
         <h2>{about.bio}</h2>
       </div>
     )
